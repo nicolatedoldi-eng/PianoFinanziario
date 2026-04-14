ed2c50f7d552bc3a4de7579c4bf284156993414d
nicolatedoldi-eng <nicolatedoldi@ioinvesto.net>
2026-04-14 11:42:25 +0200

    Add PWA: manifest, service worker, install banner, meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import InstallBanner from './components/InstallBanner.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <InstallBanner />
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => console.log('SW registered'))
+      .catch(e => console.log('SW error:', e))
+  })
+}
