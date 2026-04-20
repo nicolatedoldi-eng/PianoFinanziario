7cf1e990245bb7f05627ba11ccfe7449d5f287de
nicolatedoldi-eng <nicolatedoldi@ioinvesto.net>
2026-04-20 11:58:44 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
